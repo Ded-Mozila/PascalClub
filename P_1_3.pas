ab9db886aa57226f9c6cd90374fa551aa13b32fe
zyryanov.aleksander <zyryanov.aleksander@gmail.com>
2017-01-10 12:54:23 +1000

    Задача 3: Дано значение температуры TF в градусах Фарейнгейта. Найти и вывести значение температуры в градусах Цельсия TC.TC = (TF − 32) · 5/9

---
+++ P_1_3.pas
@@ -0,0 +1,9 @@
+program Basic_1_2;
+var Tf,Tc: real;
+BEGIN
+writeln('Введите температуру в Градусах Фарейнгейта');
+read(Tf);
+writeln('Температура в Цельсиях = ',(Tf-32)*(5/9));
+readln(Tc);
+END.
+

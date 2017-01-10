d48b2c67ff58695835673dda0d9a2ed3a34e6e44
zyryanov.aleksander <zyryanov.aleksander@gmail.com>
2017-01-10 13:28:40 +1000

    Задача 4: Дано значение температуры в градусах Цельсия. Перевести температуру в градусы по шкале Фарейнгейта.Задача 5:Даны две переменных целого типа, поменять местами их значения. Задача 6:Дана сторона квадрата. Найти его периметр и площадь. Задача 7:Найти длину окружности и площадь круга с заданным радиусом.Задача 8:Найти среднее арифметическое двух чисел a и b.Задача 9:Даны два ненулевых числа. Найти сумму, произведение, разность и частное их модулей.

---
+++ P_1_9.pas
@@ -0,0 +1,12 @@
+program Basic_1_2;
+var a,b: integer;
+BEGIN
+writeln('Введите a b');
+read(a,b);
+writeln('Сумма ',a,' и ', b,' = ',(a+b));
+writeln('Произведение ',a,' и ', b,' = ',(a*b));
+writeln('Разность ',a,' и ', b,' = ',(a-b));
+writeln('Частное ',a,' и ', b,' = ',(a/b));
+read(a);
+END.
+
